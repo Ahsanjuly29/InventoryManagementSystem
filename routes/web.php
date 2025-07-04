0ffc816bcf2f1f196499147ec5769a0ac648952d
ahsanjuly29 <ahsan.july29@gmail.com>
2025-07-05 04:28:04 +0600

    up

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ Route::get('/', fn() => redirect('/reports'));
 
 Route::middleware('auth')->group(function () {
 
-    Route::get('/dashboard', fn() => redirect('/reports'));
+    Route::get('/dashboard', [ReportController::class, 'index']);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
